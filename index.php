968ccd06dc446ee165df0eaeef7260dcd38d3e76
Philip Cristobal <philipcristobal06@gmail.com>
2023-11-09 10:44:11 +0800

    Display questions and choices

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    require_once('db/db.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,36 @@
     <title>Exam</title>
 </head>
 <body>
-    
+    <div class="questions-container">
+        <?php
+            $sql = "Select * from tblquestions";
+            $result = mysqli_query($con, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                $question = $row['questions'];
+                $opt1 = $row['opt1'];
+                $opt2 = $row['opt2'];
+                $opt3 = $row['opt2']; 
+        ?>
+        <div class="question">
+            <?php echo $question;?>
+        </div>
+        <div class="choices">
+            <div class="form">
+                <input type="radio" name="choice" id="opt1" value="<?php echo $opt1;?>">
+                <label class="choice-desc" for="opt1">A. <?php echo $opt1; ?></label>
+            </div>
+           <div class="form">
+                <input type="radio" name="choice" id="opt2" value="<?php echo $opt2;?>">
+                <label class="choice-desc" for="opt2">B. <?php echo $opt2; ?></label>
+           </div>
+           <div class="form">
+                <input type="radio" name="choice" id="opt3" value="<?php echo $opt3;?>">
+                <label class="choice-desc" for="opt3">C. <?php echo $opt3; ?></label>
+           </div>
+        </div>
+        <?php
+            }
+        ?>
+    </div>
 </body>
 </html>
