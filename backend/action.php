161fd5f0e578f54ef7c7408282cf0b57d28c4a64
Philip Cristobal <philipcristobal06@gmail.com>
2023-11-09 14:38:42 +0800

    Add login validation

--- backend/action.php
+++ backend/action.php
@@ -8,11 +8,21 @@ if(isset($_POST['q_id'])) {
 
 if(isset($_POST['access'])) {
     $name = $_POST['name'];
-    $sql = "Insert into tblusers(name)values('$name')";
+    $sql = "Select * from tblusers where name = '$name'";
     $result = mysqli_query($con, $sql);
-    if($result) {
-        $_SESSION['name'] = $name;
-        header('location:../index.php');
+    $row_count = mysqli_num_rows($result);
+    if($row_count == 1) {
+            $_SESSION['status'] = "User Already Access";
+            $_SESSION['status_code'] = "error";  
+            $_SESSION['status_text'] = "Please try again";
+            header("location:../login.php");
+    } else {
+        $sql = "Insert into tblusers(name)values('$name')";
+        $result = mysqli_query($con, $sql);
+        if($result) {
+            $_SESSION['name'] = $name;
+            header('location:../index.php');
+        }
     }
 }
 ?>
--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,12 +13,30 @@
     <div class="wrapper">
         <div class="container">
         <form method="POST" action="backend/action.php">
-            <input type="text" class="name-input" name="name" placeholder="Enter Your Name">
+            <input type="text" class="name-input" name="name" placeholder="Enter Your Name" required>
             <input type="submit" class="submit-btn" name="access" value="Access Questions">
         </form>
         </div>
     </div>
+
     <script src="assets/sweetalert/jquery-3.6.0.min.js"></script>
     <script src="assets/sweetalert/sweetalert2.all.min.js"></script>
+    <script>
+        <?php
+            if(isset($_SESSION['status'])) {
+        ?>    
+            swal.fire({
+                icon: '<?php echo $_SESSION['status_code']; ?>',
+                title: '<?php echo $_SESSION['status']; ?>',
+                text: '<?php echo $_SESSION['status_text']; ?>'
+            })
+        <?php 
+            unset($_SESSION['status']);
+            unset($_SESSION['status_code']);
+            unset($_SESSION['status_text']);
+        }
+        
+        ?>
+    </script>
 </body>
 </html>
